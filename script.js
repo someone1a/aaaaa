3f318b388ce7cd9caa8b8cac37189524212d5258
Walter Carrasco <95136445+someone1a@users.noreply.github.com>
2024-01-31 16:49:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ let numbers = "1234567891"
 const app = (numbers) => {
     let sortNumbers = Math.floor(Math.random() * numbers.length)
     var randomNumber = numbers.charAt(sortNumbers);
-    console.log(randomDigit);
     let userAnswer = document.getElementById("userAnswer").value
-    if (userAnswer === randomDigit){
+    if (userAnswer === randomNumber){
         alert("Felicidades, acertaste")
     }else(
-        alert(`upsss, le erraste. el numero era ${randomDigit}`)
+        alert(`upsss, le erraste. el numero era ${randomNumber}`)
     )
 }
